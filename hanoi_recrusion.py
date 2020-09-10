6f0047e86c027730da8c977d63ba1001ca622736
Mdwsbb01 <Mdwsbb01@gmail.com>
2020-09-10 09:39:42 -0400

    hanoi problem

---
+++ hanoi_recrusion.py
@@ -0,0 +1,11 @@
+#recrusion example: hanoi
+def haNoi(n, a, b, c):
+    if n > 0:
+        haNoi(n-1, a, c, b)
+        print("moving from %s to %s" % (a, c))
+        haNoi(n-1, b, a, c)
+
+haNoi(3, 'A', 'B', 'C')
+
+#equation:h(n) = h(n-1) + 1 + h(n-1)
+#h(x) = 2h(x-1) + 1 ~~ 2^n
